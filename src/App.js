7fb7b7874904cfb90774f3da05bd4c5fdae7503d
Daidonm <phet.lue@gmail.com>
2026-08-03 10:57:37 +0700

    EventData validation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import  {Header,Content, Footer } from './func-components';
 import Calendar from './class-component';
 import Calculator2 from './calculator';
+import EventData2 from './event-data';
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;

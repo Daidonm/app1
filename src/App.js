836335394db87b3578742d9f630c37e64d0b5ea9
Daidonm <phet.lue@gmail.com>
2026-08-03 10:53:38 +0700

    Revert "validation EventData"
    This reverts commit a89c335d15e70dbc708aa95bf5708333201244b1.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import  {Header,Content, Footer } from './func-components';
 import Calendar from './class-component';
 import Calculator2 from './calculator';
-import EventData2 from './event-data';
 
 function App() {
-  return <EventData2/>
+  return <Calculator2/>
 }
 
 export default App;

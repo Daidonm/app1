caebde68fff6a3bd4916e629c68ce74932d5418b
Daidonm <phet.lue@gmail.com>
2026-08-10 10:06:19 +0700

    ref-fucntion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import  {Header,Content, Footer } from './func-components';
-import Calendar from './class-component';
-import Calculator2 from './calculator';
-import EventData2 from './event-data';
+import RefsFunc from './refs-func';
 
-function App() {
-  return <EventData2/>
+export default function App() {
+  return RefsFunc();
 }
 
-export default App;
